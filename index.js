d9e323d68397a8b8292fbb60e1c3001d9aee94a1
davidbelleza <davidbelleza@gmail.com>
2020-07-26 15:39:11 -0500

    add page-template and generate page

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const generatePage = require('./src/page-template');
+
 
 // employee array
 const employees = [];
@@ -63,7 +65,18 @@ const addManager = () => {
                 }
             }
         ])
-        .then(() => {
+        .then(data => {
+            const{name, id, email, office} = data;
+            const manager = new Manager(name, id, email, office);
+            const managerOutput = {
+                role: manager.getRole(),
+                name: manager.getName(),
+                id: manager.getId(),
+                email: manager.getEmail(),
+                info: manager.getOffice()
+            }
+
+            employees.push(managerOutput);
             addTeam();
         })
 }
@@ -76,7 +89,7 @@ const addTeam = () => {
                 type    : 'list',
 				name    : 'menuChoice',
 				message : "Please enter the next team member's position or finish building your team",
-				choices : [ 'Engineer', 'Intern', 'Finish' ]
+				choices : ['Engineer', 'Intern', 'Finish']
             }
         ])
         .then( data => {
@@ -85,7 +98,19 @@ const addTeam = () => {
             }else if(data.menuChoice === 'Intern'){
                 addIntern();
             }else{
-                console.log('finishing team build');
+                const pageHTML = generatePage(employees);
+
+                return writeFile(pageHTML)
+                        .then(writeFileResponse => {
+                            console.log(writeFileResponse);
+                            return copyFile();
+                        })
+                        .then(copyFileResponse => {
+                            console.log(copyFileResponse);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
             }
         })
 }
@@ -139,7 +164,18 @@ const addEngineer = () => {
                 }
             }
         ])
-        .then(() => {
+        .then(data => {
+            const{name, id, email, github} = data;
+            const engineer = new Engineer(name, id, email, github);
+            const engineerOutput = {
+                role: engineer.getRole(),
+                name: engineer.getName(),
+                id: engineer.getId(),
+                email: engineer.getEmail(),
+                info: engineer.getGithub()
+            }
+
+            employees.push(engineerOutput);
             addTeam();
         })
 }
@@ -193,9 +229,55 @@ const addIntern = () => {
                 }
             }
         ])
-        .then(() => {
+        .then(data => {
+            const{name, id, email, school} = data;
+            const intern = new Intern(name, id, email, school);
+            const internOutput = {
+                role: intern.getRole(),
+                name: intern.getName(),
+                id: intern.getId(),
+                email: intern.getEmail(),
+                info: intern.getSchool()
+            }
+
+            employees.push(internOutput);
             addTeam();
         })
+}
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile('./dist/index.html', fileContent, err => {
+        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+          reject(err);
+          // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+          return;
+        }
+  
+        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+        resolve({
+          ok: true,
+          message: 'File created!'
+        });
+      });
+    });
+};
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'Style sheet copied successfully!'
+            })
+        });
+    })
 }
 
 addManager();
---
+++ src/page-template.js
@@ -0,0 +1,55 @@
+const createCard = data => {
+    const outputArray = data.map(property => {
+        const {role, name, id, email, info} = property;
+        return `
+        <div class="card mx-3">
+            <div class="card-header bg-info text-white p-3">
+                <h5 class="card-title">${name}</h5>
+                <h6 class="card-title">${role}</h6>
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">EMAIL: <a href="mailto:${email}">${email}</a></li>
+                    <li class="list-group-item">${info}</li>
+                </ul>
+            </div>
+        </div>
+        `;
+    });
+
+    return outputArray.join('\n');
+}
+
+module.exports = data => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <title>TEAM PROFILE</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                    <div class="col-12 bg-dark text-white py-3 mb-4">
+                        <h1 class="text-center">TEAM PROFILE</h1>
+                    </div>
+            </div>
+            <div class="row">
+                <div class="col-12 justify-content-center d-flex flex-wrap p-3">
+                    ${createCard(data)}
+                </div>
+            </div>
+        </div>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    </body>
+    </html>
+    `;
+}
